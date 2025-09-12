1dd3bcdba80cb2b37d09b8a00d6dce8cf3f49ec2
Gopari supriya <Gopari supriya@Goparisupriya>
2025-09-12 21:45:20 +0530

    Even or Odd

---
+++ first/src/com/tnsif/second/Evenodd.java
@@ -0,0 +1,14 @@
+package com.tnsif.second;
+
+public class Evenodd {
+
+	public static void main(String[] args) {
+        int num = 7;  // you can change the value here
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even.");
+        } else {
+            System.out.println(num + " is Odd.");
+        }
+    }
+}
