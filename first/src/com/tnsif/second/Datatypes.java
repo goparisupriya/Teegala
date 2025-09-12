a3f7ef98269e79ba3435a85c7833d4d45ad0197c
Gopari supriya <Gopari supriya@Goparisupriya>
2025-09-12 20:18:34 +0530

    second commit

---
+++ first/src/com/tnsif/second/Datatypes.java
@@ -0,0 +1,41 @@
+package com.tnsif.second;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num = 150000;
+		
+		short a = 2200;
+		
+		byte b = 127;
+		
+		long l = 20000000000L;
+		
+		float f = 3.14f;
+		
+		double d = 3.141598;
+		
+		char c = 'S';
+		
+		boolean bool = true;
+		
+		System.out.println("Integer : "+num);
+		System.out.println("Short : "+a);
+		System.out.println("Byte : "+b);
+		System.out.println("Long : "+l);
+		System.out.println("Float : "+f);
+		System.out.println("Double : "+d);
+		System.out.println("Character : "+c);
+		System.out.println("Boolean : "+bool);
+		
+		
+		String name = "Sanjay";
+		
+		int[] arr = {240,241,242,243,244};
+		
+		System.out.println("String : "+name);
+		System.out.println("Array : "+arr[0]+" "+arr[1]+" "+arr[2]+" "+arr[3]+" "+arr[4]);
+	}
+
+}
