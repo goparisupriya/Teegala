62604ef3f4f17fc47eb7883577749b35f866167e
Gopari supriya <Gopari supriya@Goparisupriya.lan>
2025-10-05 19:15:36 +0530

    prgrm

---
+++ first/src/encapsule/program.java
@@ -0,0 +1,20 @@
+package encapsule;
+
+public class program {
+
+	
+		private String CarName;
+		private String CarColor;
+		public void SetCarName(String CarName) {
+			this.CarName = CarName;
+		}
+		public String getCarName() {
+			return CarName;
+		}
+		public void SetCarColor(String CarColor) {
+			this.CarColor = CarColor;
+		}
+		public String getCarColor() {
+			return CarColor;
+		}
+	}
