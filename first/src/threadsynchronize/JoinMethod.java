c8f3d4cdee758d359459ee5bc6bf39277577c2ee
Gopari supriya <Gopari supriya@Goparisupriya.lan>
2025-10-06 12:11:50 +0530

    JOin Method

---
+++ first/src/threadsynchronize/JoinMethod.java
@@ -0,0 +1,20 @@
+package threadsynchronize;
+
+public class JoinMethod {
+	
+		private int threadNumber;
+		public JoinMethod(int threadNumber) {
+			this.threadNumber = threadNumber;
+		}
+		
+		public void run() {
+			for(int i=1; i<=5;i++)
+			System.out.println(i + " from thread Number" + threadNumber);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				System.out.println(e);
+			}
+
+}
+}
